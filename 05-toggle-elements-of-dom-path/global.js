aeea451970488ef1145f52ee6a315975a1ea245f
Ar4is <artursvaluks@inbox.lv>
2021-03-23 16:26:27 +0200

    5 commit + ansywers

--- 05-toggle-elements-of-dom-path/global.js
+++ 05-toggle-elements-of-dom-path/global.js
@@ -1,1 +1,20 @@
 // Your JavaScript goes here.
+window.addEventListener('load', function () {
+
+    const ELEMENTS = document.querySelectorAll(".second_five li")
+    const BUTTON = document.querySelector('#toggle_button')
+
+    var toggleStatus = false
+    BUTTON.addEventListener('click', function (e) {
+        ELEMENTS.forEach(function (element) {
+            if (toggleStatus == false) {
+                element.style.display = 'flex'
+            }
+            else
+            {
+                element.style.display = 'none'
+            }
+        })
+        toggleStatus = !toggleStatus
+    })
+})
